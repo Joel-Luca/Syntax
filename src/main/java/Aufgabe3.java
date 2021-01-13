ae912487b0563cd9908895f5a327e70b0fa1ec01
Joel-Luca <joel.christinet@gmx.ch>
2021-01-13 15:41:45 +0100

    Aufgabe3 erledigt

--- src/main/java/Aufgabe3.java
+++ src/main/java/Aufgabe3.java
@@ -1,7 +1,10 @@
 public class Aufgabe3 {
     public static void main(String[] args) {
-        for(int i = 0; i < 3; i++) {
-            System.out.println("*");
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
         }
     }
 }
